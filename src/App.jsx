eb984f15a4bb3cbd55c32b96f2c22d2b7a30973c
Valeriia <valeriia.plokha@gmail.com>
2025-09-11 19:37:06 +0200

    Mina ändringar i App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import HelloJson from './components/HelloJson'
+import UpdateDoc from "./components/UpdateDoc";
 
 function App() {
 
   return (
     <>
     <h1><HelloJson/></h1>
+    <h2>Test av Update</h2>
+    <UpdateDoc id={1} />
     </>
   )
 }

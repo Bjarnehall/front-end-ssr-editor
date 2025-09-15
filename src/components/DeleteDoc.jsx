2ca114d8b96ef2c83734edb54b33f4c0fe3485a9
Bjarnehall <Jonas.bjarnehall@outlook.com>
2025-09-15 10:00:29 +0200

    removed fetch from delete doc since its no longer needed after adding editor mode

--- src/components/DeleteDoc.jsx
+++ src/components/DeleteDoc.jsx
@@ -1,37 +1,14 @@
 
-function DeleteDoc() {
-
-  fetch("http://localhost:8080/all")
-        .then(res => res.json())
-        .then(data => {
-            const select = document.getElementById("dokument");
-
-            for (let i = 0; i < data.data.length; i++) {
-                console.log(data.data[i].title + "ID: " + data.data[i].id);
-                const option = document.createElement("option");
-                option.value = data.data[i].id;
-                option.text = data.data[i].title;
-                select.appendChild(option);
-
-            }
-        });
+function DeleteDoc({ preselectedDoc }) {
 
   return (
-
-    <>
     <div className="delete-doc-form">
-
-    
-    <form method="POST" action="http://localhost:8080/delete">
-    <h4>Här kan du kasta befintliga dokument</h4>
-        <label htmlFor="dokument">Välj ett dokument</label><br/>
-        <select name="dokument" id="dokument">
-        </select>
-
+      <form method="POST" action="http://localhost:8080/delete">
         <input type="submit" value="Kasta dokument" />
-    </form>
+        <input type="hidden" name="dokument" value={preselectedDoc ? preselectedDoc.id : selectedId} />
+      </form>
     </div>
-    </>
-  )
+  );
 }
-export default DeleteDoc
+
+export default DeleteDoc;

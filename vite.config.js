70a1d138ae8ec66580e08df87736278a00ac6184
Bjarnehall <Jonas.bjarnehall@outlook.com>
2025-09-23 07:58:12 +0200

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/front-end-ssr-editor/',
   test: {
     environment: "jsdom",
     globals: true,
